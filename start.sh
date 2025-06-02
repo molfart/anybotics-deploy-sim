9bd382501d18b18e33c8fc63b76b3c87d129fad4
mwolfart <maxime.wolfart@web.de>
2025-06-02 18:08:39 +0200

    Provision and start nginx and echo_service via Ansible

--- start.sh
+++ start.sh
@@ -6,12 +6,19 @@ if ! command -v docker compose &> /dev/null; then
   exit 1
 fi
 
+# Option for a clean build and ansible provisioning
+if [[ "$1" == "clean" ]]; then
+  echo "Performing clean rebuild..."
+  docker compose down --volumes --remove-orphans
+  docker compose build
+fi
+
 echo "Starting ANYbotics Deployment Simulation Setup..."
 
 echo "Bringing up simulation containers (node_a, node_b)..."
 docker compose up -d
 
-# --- Wait briefly to ensure containers are ready ---
+# Wait briefly to ensure containers are ready
 sleep 2
 
 echo "Starting development environment container and running ansible-playbook"
@@ -21,3 +28,4 @@ docker compose exec dev ansible-playbook ansible/playbooks/base_setup.yml
 # Run ansible-playbook commands here once playbooks are ready
 
 echo "Setup complete."
+exit 0
